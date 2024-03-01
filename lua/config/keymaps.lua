e8f44c48cbcf9f3fdfe3223ea0a72cb5ee0dc4a8
kenbio216 <3498592827@qq.com>
2024-03-01 14:21:29 +0800

    111

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,5 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<Esc>")
 
 keymap.set("n", "<leader>sx", require("telescope.builtin").resume, { noremap = true, silent = true, desc = "Resume" })
+
+keymap.set("n", "<F8>", ":MarkdownPreview<CR>", { noremap = true, silent = true, desc = "markdownpreview" })
